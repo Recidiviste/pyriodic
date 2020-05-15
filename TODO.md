56cbeb89e1f165c368b1e1a3a9ca9167bcf7c8ca
Recidivste <luc.saccoccio@gmail.com>
2020-05-15 23:34:05 +0200

    database part of the project is now gone;adapted for the go api (faster and up while python one is down); rewrote README.md; added TODO.md

---
+++ TODO.md
@@ -0,0 +1,16 @@
+# Todo List
+
+## Getting Data
+
+- [ ] Get by different Data Types
+  - [ ] Atomic Number
+  - [ ] Atomic Name
+  - [ ] Atomic Symbol
+
+## Interface
+
+- [ ] Add menus
+  - [ ] Main Menu
+  - [ ] Type of searching menu
+  - [ ] Searchs Menus
+- [ ] Add help for each menu
--- pyriodic_data.py
+++ pyriodic_data.py
@@ -1,38 +1,41 @@
 import json
 import requests
 
-class Element(dico):
+class Element():
     def __init__(self, dico):
         self.symbol = dico['symbol']
         self.name = dico['name']
-        self.mass = dico['atomicMass']
-        self.number = dico['']
-        self.radius = dico['']
-        self.boilingPoint = dico['']
-        self.bondingType = dico['']
-        self.density = dico['']
-        self.electronAffinity = dico['']
-        self.electronegativity = dico['']
-        self.electronicConfiguration = dico['']
-        self.groupBlock = dico['']
-        self.ionRadius = dico['']
-        self.ionizationEnergy = dico['']
-        self.meltingPoint = dico['']
-        self.oxidationStates = dico['']
-        self.standardState = dico['']
-        self.vanDerWaalsRadius = dico['']
-        self.yearDiscovered = dico['']
+        self.atomicMass = dico['atomicMass']
+        self.atomicNumber = dico['atomicNumber']
+        self.atomicRadius = dico['atomicRadius']
+        self.boilingPoint = dico['boilingPoint']
+        self.bondingType = dico['bondingType']
+        self.density = dico['density']
+        self.electronAffinity = dico['electronAffinity']
+        self.electronegativity = dico['electronegativity']
+        self.electronicConfiguration = dico['electronicConfiguration']
+        self.groupBlock = dico['groupBlock']
+        self.ionRadius = dico['ionRadius']
+        self.ionizationEnergy = dico['ionizationEnergy']
+        self.meltingPoint = dico['meltingPoint']
+        self.oxidationStates = dico['oxidationStates']
+        self.standardState = dico['standardState']
+        self.vanDerWaalsRadius = dico['vanDelWaalsRadius']
+        self.yearDiscovered = dico['yearDiscovered']
 
 
 def get_info(parameter):
-    url = "https://neelpatel05.pythonanywhere.com/element/atomicnumber"
-    raw = requests.get(url, parameter)
+    url = "https://periodic-table-api.herokuapp.com/atomicNumber"
+    raw = requests.get("/".join([url, parameter]))
     if raw.status_code != 200:
         print(raw.status_code)
         return 1
-    jformatted = json.dumps(raw.json(), indent=4)
-    print(type(jformatted))
-    print(type(raw.json()))
+    else:
+        return raw.json()
 
-parameter = {"atomicnumber":input("What number ? :")}
-get_info(parameter)
+if __name__ == "__main__":
+    parameter = input("What number ? :")
+    telement = Element(get_info(parameter))
+    exec( telement.name + " = telement ")
+    del telement
+    print(Hydrogen.density)
